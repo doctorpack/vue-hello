1bcfa608f144f562f599de8efecb5d2dbe38a188
doctorpack <enricopachera@gmail.com>
2022-11-13 22:47:59 +0100

    fix exercise

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            message: "HELLO WORLD",
+            red: "red",
+            image: "img/moto.jpg"
+        }
+    }
+);
